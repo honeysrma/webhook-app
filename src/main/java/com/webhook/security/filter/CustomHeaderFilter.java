6eeb009c9028e1101e66531e67ed1c9c878963d2
honeysrma <honeysrma@gmail.com>
2023-08-14 18:43:39 +0530

    add extra customise headers

---
+++ src/main/java/com/webhook/security/filter/CustomHeaderFilter.java
@@ -0,0 +1,48 @@
+package com.webhook.security.filter;
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component
+@WebFilter("/*") // Apply the filter to all URLs
+@Order(Ordered.LOWEST_PRECEDENCE - 1)
+public class CustomHeaderFilter implements Filter {
+
+	
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+        HttpServletResponse httpResponse = (HttpServletResponse) response;
+
+        // Add the desired headers to the response
+        httpResponse.setHeader("Access-Control-Allow-Origin", "*");
+        httpResponse.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        httpResponse.setHeader("Access-Control-Allow-Headers", "*");
+        httpResponse.setHeader("Access-Control-Allow-Credentials", "true");
+        httpResponse.setHeader("X-Frame-Options", "DENY");
+        
+        // Continue the filter chain
+        chain.doFilter(request, httpResponse);
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+        // Initialization logic if needed
+    }
+
+    @Override
+    public void destroy() {
+        // Cleanup logic if needed
+    }
+}
